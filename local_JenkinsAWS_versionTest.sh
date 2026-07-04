c01bb9c15199e40f7b437a6e9f96901699e60994
Ashish <cloudtechakp@gmail.com>
2026-07-04 12:31:33 +0400

    Update local_JenkinsAWS_versionTest.sh

--- local_JenkinsAWS_versionTest.sh
+++ local_JenkinsAWS_versionTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #echo $((1+2))
-echo "We are checking for Aws version in our windows machine"
+echo "We are checking for Aws,git and java version in our windows machine"
 aws --version
 git --version
 java -version
